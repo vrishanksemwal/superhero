2678b6df7f100410dbadee3b770218c8c082b627
vrishanksemwal <80450599+vrishanksemwal@users.noreply.github.com>
2021-06-29 01:56:02 -0700

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,26 @@
+class Monster{
+    constructor(x,y,r)
+    {
+        var options={
+            density:5,
+            frictionAir:1
+        };
+        this.x=x
+        this.y=y
+        this.r=r
+        this.image=loadImage("monster3.png")
+        this.body=Bodies.circle(this.x,this.y,(this.r)/2,options)
+        World.add(world,this.body)
+    }
+    display()
+    {
+        var monsterPos=this.body.position
+        push()
+        translate(monsterPos.x,monsterPos.y-100)
+        rectMode(CENTER)
+        fill(255)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r,this.r)
+        pop()
+        }
+    }
